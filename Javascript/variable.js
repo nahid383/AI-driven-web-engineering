7d6dc7f56a503c9d999c20005f87a348d824f63b
nahid383 <nahid1719abd@gmail.com~>
2026-07-16 20:56:04 +0600

    added variable.js

---
+++ Javascript/variable.js
@@ -0,0 +1,21 @@
+// variable
+var weight = 55;
+var distance = 464;
+var temperature = 30;
+
+console.log(distance, temperature, weight);
+
+//let, const
+let price = 100;
+price = 200;
+price = 150;
+
+console.log(price);
+
+const PI = 3.1416;
+
+console.log(PI);
+
+//string
+let name  = "Shahjalal University of Science and Technology, Sylhet";
+console.log(name);
