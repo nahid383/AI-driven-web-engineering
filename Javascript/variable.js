5e51692b9c1d82ad3390eabd157a562711b87864
nahid383 <nahid1719abd@gmail.com~>
2026-07-16 21:00:14 +0600

    added more data types

--- Javascript/variable.js
+++ Javascript/variable.js
@@ -11,11 +11,19 @@ price = 200;
 price = 150;
 
 console.log(price);
+console.log(typeof price)
 
 const PI = 3.1416;
 
 console.log(PI);
+console.log(typeof PI);
 
 //string
 let name  = "Shahjalal University of Science and Technology, Sylhet";
 console.log(name);
+console.log(typeof name);
+
+//boolean
+const isRich = false;
+console.log(isRich);
+console.log(typeof isRich);
