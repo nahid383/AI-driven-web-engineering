0b021f3c352e0fd6359fcfb86ee6c963490375eb
nahid383 <nahid1719abd@gmail.com~>
2026-07-17 09:57:37 +0600

    mathematical operation added

---
+++ Javascript/mathoperation.js
@@ -0,0 +1,29 @@
+//Summation
+const nahidTaka = 2790;
+const rayedTaka = 7920;
+const totalTaka = nahidTaka + rayedTaka;
+console.log("Total amount of taka : " , totalTaka);
+
+//Subtraction
+const salehBalance = 5899;
+const ankitBalance = 2330;
+const difference = salehBalance - ankitBalance;
+console.log("Difference of their balance : " , difference);
+
+//Multiplication
+const mangoPrice = 95;
+const quantity = 7;
+const totalPrice = mangoPrice * quantity;
+console.log("Total price of mango : " , totalPrice);
+
+//Division
+const totalLichi = 250;
+const person = 30;
+const lichiPerPerson = totalLichi/person;
+console.log("Quantity of lichi per person got : " , lichiPerPerson);
+
+//Remainder
+const dividend = 380;
+const divisor = 23;
+const remainder = dividend % divisor;
+console.log("Remainder of the division : " , remainder);
