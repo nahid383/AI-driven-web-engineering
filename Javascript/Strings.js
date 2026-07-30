a2e7cc2bf6854a21c9ebb32a9b9b8598db328580
nahid383 <nahid1719abd@gmail.com~>
2026-07-30 23:13:44 +0600

    strings generation

---
+++ Javascript/Strings.js
@@ -0,0 +1,20 @@
+const goals = 4;
+const player = 'mess e';
+const cup = 'world cup';
+const location = `Bandor Ban`;
+
+const numbers = [4, 54, 6, 78, 11];
+
+console.log(numbers.length);
+console.log(location);
+console.log(location.length);
+
+const subject = "Shahjalal University of Science and Technology";
+console.log(subject.toUpperCase());
+
+const first = 'Nahid ';
+const last = 'Hasan';
+
+console.log(first+last);
+
+console.log(cup.includes("cup"));
