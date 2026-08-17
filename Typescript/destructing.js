a7a6c787a1b836439778c5c57c705bc6ef3e9652
nahid383 <nahid1719abd@gmail.com~>
2026-08-17 21:45:43 +0600

    added destructing

---
+++ Typescript/destructing.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'Nahid Hasan',
+    age: 21,
+    isAvailable: true
+};
+// const age = user.age
+// const name = user.name
+// const {age, name} = {name:'Nahid Hasan', age:21}
+const { age, name } = user;
+console.log(age, name);
+export {};
